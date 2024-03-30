20b903535f729ecf5ed311d9a9dc11b194d619dd
sudhanshuydv11 <sudhanshu.sy2000@gmail.com>
2024-03-30 13:20:48 +0530

    adding table

--- src/client/components/app/app.js
+++ src/client/components/app/app.js
@@ -1,41 +1,18 @@
 import React from "react";
 import DataTable from 'react-data-table-component';
+import {getColumns, getData} from '../../utils/table-data'
 import "./styles.scss";
-
-const columns = [
-	{
-		name: 'Title',
-		selector: row => row.title,
-	},
-	{
-		name: 'Year',
-		selector: row => row.year,
-	}
-];
-
-const data = [
-  	{
-		id: 1,
-		title: 'Beetlejuice',
-		year: '1988',
-	},
-	{
-		id: 2,
-		title: 'Ghostbusters',
-		year: '1984',
-	},
-   
-]
 
 const App = ({}) => {
  
+	const columns = getColumns();
 
-   return <div className="test-class">
-                <DataTable
+	const data = getData();
+
+   return <DataTable
 			columns={columns}
 			data={data}
-		/>
-   </div>;
+		/>;
 };
 
 export default App;
---
+++ src/client/utils/table-data.js
@@ -0,0 +1,43 @@
+const NO_OF_COLUMNS_AND_ROWS=100;
+const COLUMN_HEADING='Title';
+export const getColumns =  () => {
+let allColumns=[];
+
+for(let i=0; i< NO_OF_COLUMNS_AND_ROWS;i++){
+	allColumns.push(
+		{
+			name: `${COLUMN_HEADING}${i}`,
+			selector: row => row[`${COLUMN_HEADING.toLowerCase()}${i}`]
+		}
+	);
+}
+
+return allColumns;
+};
+
+export const getData = () => {
+
+	const getSingleRow=(rowIndex)=>{
+		let singleRow={};
+		for(let i=0; i< NO_OF_COLUMNS_AND_ROWS;i++){
+			singleRow={
+				...singleRow,
+				[`${COLUMN_HEADING.toLowerCase()}${i}`]: `${rowIndex}Beetlejuice${i}`
+			}
+		}
+		return singleRow;
+	}
+   
+	let allRows=[];
+
+for(let i=0; i< NO_OF_COLUMNS_AND_ROWS;i++){
+	allRows.push(
+		{
+			id: i,
+			...getSingleRow(i)
+		}
+	);
+}
+
+return allRows;
+};
